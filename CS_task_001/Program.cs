8ff534274b4282ad46199206dfc151fe730a171a
rn4nv <dorjuan@ya.ru>
2022-12-05 16:11:16 +0700

    Completed task 001

---
+++ CS_task_001/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+string a = Console.ReadLine();
+int resultA = int.Parse(a);
+
+if (resultA >= 100 && resultA < 1000)
+{
+    resultA = (resultA % 100);
+    resultA = (resultA / 10);
+    Console.Write("Вторая цифра в введенном числе равна: ");
+    Console.Write(resultA);
+}
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число. Запустите программу заново и введите трехзначное число.");
+}
