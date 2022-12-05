dee734e4652168bb60ecf24d338fda3342cc2284
rn4nv <dorjuan@ya.ru>
2022-12-05 18:14:13 +0700

    Completed CS task 003

---
+++ CS_task_003/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+Console.Write("Введите номер, обозначающий день недели: ");
+string a = Console.ReadLine();
+int resultA = int.Parse(a);
+
+if (resultA > 0 && resultA < 8)
+{
+    if (resultA >= 1 && resultA <= 5)
+    {
+        Console.WriteLine("Нет, данный день недели не является выходным днём, это рабочий день.");
+    }
+    else
+    {
+        Console.WriteLine("Да, данный день недели является выходным днём");
+    }
+}
+else
+{
+    Console.WriteLine("Введенный номер дня недели не существует.");
+    Console.WriteLine("Запустите программу заново и введите существующий номер дня недели.");
+}
